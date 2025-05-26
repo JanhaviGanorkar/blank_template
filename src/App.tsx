145af71bad0578586350628604208d687bb89951
janhaviganorkar <janhaviganorkar12@gmail.com>
2025-05-26 19:54:07 +0530

    tem

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Button } from "@/components/ui/button"
+ 
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <h1 className='bg-amber-800'>hello</h1>
+      <h3 className="bg-amber-300">adf</h3>
+        <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
