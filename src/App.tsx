9fe59b908360b59568ff86ff2105e9d922478bc4
janhaviganorkar <janhaviganorkar12@gmail.com>
2025-05-26 20:00:48 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+// import React from "react"
 import { Button } from "@/components/ui/button"
  
 import './App.css'
@@ -11,6 +11,7 @@ function App() {
     <div>
       <h1 className='bg-amber-800'>hello</h1>
       <h3 className="bg-amber-300">adf</h3>
+      <p>trying to make build</p>
         <Button>Click me</Button>
     </div>
     </>
